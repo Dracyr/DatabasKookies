c9290a71a5d588eb540897c3db8db3b1f6a83e91
Per-Victor Persson <dracyr@gmail.com>
2016-03-31 08:38:07 +0200

    Can deliver more ingredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,12 @@
 class Ingredient < ActiveRecord::Base
+  has_many :products, through: :product_ingredients
+  has_many :product_ingredients
+
+  def deliver_ingredient(ingredient_params)
+    update({
+      quantity: self.quantity + ingredient_params[:last_delivered_quantity].to_i,
+      last_delivered_quantity: ingredient_params[:last_delivered_quantity],
+      last_delivered: DateTime.current
+    })
+  end
 end
--- db/migrate/20160331051229_create_product_ingredients.rb
+++ db/migrate/20160331051229_create_product_ingredients.rb
@@ -3,7 +3,7 @@ class CreateProductIngredients < ActiveRecord::Migration
     create_table :product_ingredients do |t|
       t.belongs_to :product, index: true
       t.belongs_to :ingredient, index: true
-      t.string :quantity
+      t.integer :quantity
     end
     add_foreign_key :product_ingredients, :products
     add_foreign_key :product_ingredients, :ingredients
