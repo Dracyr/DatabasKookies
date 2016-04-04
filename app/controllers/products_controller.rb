70728c839024665738c8272fdd782a98606cff3b
Oskar Holmberg <oskarholmberg@msn.com>
2016-04-04 16:38:30 +0200

    Add possibility to add ingredients to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name)
+      params.require(:product).permit(:name, product_ingredients_attributes: [:ingredient_id, :quantity, :id, :_destroy])
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,6 @@ class Product < ActiveRecord::Base
   has_many :product_ingredients
   has_many :ingredients, through: :product_ingredients
 
-  accepts_nested_attributes_for :product_ingredients
+  accepts_nested_attributes_for :product_ingredients, allow_destroy: true
 end
 
